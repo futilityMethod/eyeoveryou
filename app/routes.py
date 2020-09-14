e5d81174882d1208627780187b8d35dc5a0269f0
Kathryn <kknauth@amazon.com>
2020-09-13 17:40:02 -0700

    Introducing Flask for site content

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from app import app
+from app.widgets.ip_info_client import get_ip_info
+from app.widgets.metaweather import get_weather
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    ip_info = get_ip_info('151.101.65.69')
+    weather = get_weather(ip_info.all['loc'])
+    return render_template('index.html', title='EyeOverYou', ip_info=ip_info, weather=weather)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
